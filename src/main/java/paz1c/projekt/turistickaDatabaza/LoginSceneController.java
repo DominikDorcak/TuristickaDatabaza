33dba97f67c25d95763be7a526c1d5662cf364cb
dominik <dominik.dorcak@gmail.com>
2017-11-29 21:53:13 +0100

    Pridana funkcionalita prihlasovacieho okna(nefunguje!!)

--- src/main/java/paz1c/projekt/turistickaDatabaza/LoginSceneController.java
+++ src/main/java/paz1c/projekt/turistickaDatabaza/LoginSceneController.java
@@ -1,4 +1,3 @@
-
 package paz1c.projekt.turistickaDatabaza;
 
 import java.net.URL;
@@ -7,16 +6,20 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import paz1c.projekt.turistickaDatabaza.database.Pouzivatel;
+import paz1c.projekt.turistickaDatabaza.models.PouzivatelFxModel;
 
 class LoginSceneController {
-    
+
+    private PouzivatelFxModel pouzivatelModel = new PouzivatelFxModel();
+
     @FXML
     private ResourceBundle resources;
 
     @FXML
     private URL location;
 
-     @FXML
+    @FXML
     private TextField loginTextField;
 
     @FXML
@@ -28,11 +31,21 @@ class LoginSceneController {
     @FXML
     private PasswordField hesloPasswordField;
 
-
-
     @FXML
     void initialize() {
+        loginTextField.textProperty().bindBidirectional(pouzivatelModel.loginProperty());
         
+        hesloPasswordField.textProperty().bindBidirectional(pouzivatelModel.hesloProperty());
         
+        loginButton.setOnAction(eh -> {
+            Pouzivatel pouzivatel = DaoFactory.INSTANCE.getPouzivatelDao().getByLogin(pouzivatelModel.getLogin());
+            if(pouzivatel != null){
+                if(pouzivatelModel.getHeslo().equals(pouzivatel.getHeslo())){
+                    System.out.println("vyslo to!!");
+                }
+            }
+        });
+        
+
     }
 }
