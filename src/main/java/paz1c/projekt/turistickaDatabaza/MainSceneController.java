c9d09dcbd219f9ac77a9d4d6000ae324e498255d
Michal <Michal@Calculon>
2017-12-10 22:25:35 +0100

    Pridana MainScene bez funkcionality

---
+++ src/main/java/paz1c/projekt/turistickaDatabaza/MainSceneController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paz1c.projekt.turistickaDatabaza;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+
+/**
+ * FXML Controller class
+ *
+ * @author Michal
+ */
+public class MainSceneController{
+
+    @FXML
+    private ListView<?> lokacieListView;
+    @FXML
+    private Button PridatRecenziuButton;
+    @FXML
+    private Button PridatLokaciuButton;
+    @FXML
+    private Button OdhlasitButton;
+    @FXML
+    private ChoiceBox<?> ZoraditChoiceBox;
+    @FXML
+    private Button ZoraditButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @FXML
+    public void initialize() {
+        // TODO
+    }    
+    
+}
