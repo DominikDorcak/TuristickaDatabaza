4683d86d3595eea6372a57bcd398912fb06c5e12
dominik <dominik.dorcak@gmail.com>
2017-12-13 14:19:36 +0100

    zlepšené prihlasovacie okno

--- src/main/java/paz1c/projekt/turistickaDatabaza/models/LokalitaFxModel.java
+++ src/main/java/paz1c/projekt/turistickaDatabaza/models/LokalitaFxModel.java
@@ -1,26 +1,29 @@
-
 package paz1c.projekt.turistickaDatabaza.models;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.beans.property.LongProperty;
 import javafx.beans.property.SimpleLongProperty;
+
 /**
  *
  * @author Michal
  */
 public class LokalitaFxModel {
+
     private StringProperty nazov = new SimpleStringProperty();
     private StringProperty region = new SimpleStringProperty();
     private LongProperty id = new SimpleLongProperty();
     private StringProperty popis = new SimpleStringProperty();
-    
+
     public String getNazov() {
         return nazov.get();
     }
-    public StringProperty nazovProperty(){
+
+    public StringProperty nazovProperty() {
         return nazov;
     }
+
     public void setNazov(String nazov) {
         this.nazov.set(nazov);
     }
@@ -28,7 +31,8 @@ public class LokalitaFxModel {
     public String getRegion() {
         return region.get();
     }
-    public StringProperty regionProperty(){
+
+    public StringProperty regionProperty() {
         return region;
     }
 
@@ -39,15 +43,13 @@ public class LokalitaFxModel {
     public String getPopis() {
         return popis.get();
     }
-    public StringProperty popisProperty(){
+
+    public StringProperty popisProperty() {
         return popis;
     }
 
     public void setPopis(String popis) {
         this.popis.set(popis);
     }
-    
 
-    
-    
 }
