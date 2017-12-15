f5f945ea3d5ac4131383a95533f44d77a117cd10
Michal <Michal@Calculon>
2017-12-15 19:17:39 +0100

    Pridaná LocationScene - bez funkcionality

---
+++ src/main/java/paz1c/projekt/turistickaDatabaza/LocationSceneController.java
@@ -0,0 +1,36 @@
+
+package paz1c.projekt.turistickaDatabaza;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author Michal
+ */
+public class LocationSceneController{
+
+    @FXML
+    private Button cancel_button;
+    @FXML
+    private Label Lokalita_label;
+    @FXML
+    private Label region_label;
+    @FXML
+    private Label hodnotenie_label;
+    @FXML
+    private Label popis_label;
+    @FXML
+    private Button addReview_button;
+
+    @FXML
+    public void initialize() {
+     
+    }    
+    
+}
